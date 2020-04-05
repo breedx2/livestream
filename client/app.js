51934536cd753f500f5e50da8128c0d4b4cdf360
jason <jason@noisybox.net>
2020-04-04 23:25:56 -0700

    show/hide video

--- client/app.js
+++ client/app.js
@@ -5,25 +5,26 @@ const stats = require('./stats');
 console.log('Client app is starting...');
 run();
 
+const streamUrl = 'http://livestream.0x77.net/hls/stream.m3u8';
+
 const video = document.getElementById('video');
 if (Hls.isSupported()) {
   console.log('hls is supported');
   const hls = new Hls();
-  hls.loadSource('http://livestream.0x77.net/hls/stream.m3u8');
+  hls.loadSource(streamUrl);
   hls.attachMedia(video);
   hls.on(Hls.Events.MANIFEST_PARSED, function() {
     console.log('manifest parsed!');
     console.log(video);
-//      video.play();
   });
 }
-// hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
-// When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element through the `src` property.
-// This is using the built-in support of the plain video element, without using hls.js.
-// Note: it would be more normal to wait on the 'canplay' event below however on Safari (where you are most likely to find built-in HLS support) the video.src URL must be on the user-driven
-// white-list before a 'canplay' event will be emitted; the last video event that can be reliably listened-for when the URL is not on the white-list is 'loadedmetadata'.
 else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-  video.src = 'http://livestream.0x77.net/hls/stream.m3u8';
+  // hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
+  // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element through the `src` property.
+  // This is using the built-in support of the plain video element, without using hls.js.
+  // Note: it would be more normal to wait on the 'canplay' event below however on Safari (where you are most likely to find built-in HLS support) the video.src URL must be on the user-driven
+  // white-list before a 'canplay' event will be emitted; the last video event that can be reliably listened-for when the URL is not on the white-list is 'loadedmetadata'.
+  video.src = streamUrl;
   video.addEventListener('loadedmetadata', function() {
     console.log('loadedmetadata complete')
     video.play();
@@ -34,6 +35,13 @@ else if (video.canPlayType('application/vnd.apple.mpegurl')) {
 async function checkStreams(){
   const streams = await stats.listStreams();
   console.log("We have these streams: " + streams);
+  const video = document.getElementById('video');
+  if(streams.length) {
+    video.style.display = 'inline';
+  }
+  else {
+    video.style.display = 'none';
+  }
 }
 
 async function run(){
--- client/stats.js
+++ client/stats.js
@@ -8,7 +8,6 @@ async function listStreams(){
     .then(response => response.text())
     .then(xml => {
       return new Promise((fulfill,reject) => {
-        // console.log(`This xml: ${xml}`);
         const jsonStr = xml2js.parseString(xml, (err, res) => {
           if(err){
             return reject(err);
@@ -18,10 +17,8 @@ async function listStreams(){
       });
     })
     .then(data => {
-      console.log(JSON.stringify(data));
       // HARD CODED APP NAME HERE IS "live"
       const apps = _.get(data, 'rtmp.server[0].application[0].live', []);
-      console.log(apps);
       return apps.flatMap(app => _.get(app, 'stream[0].name[0]'));
     });
 }
